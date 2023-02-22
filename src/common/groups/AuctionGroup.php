dd001dfb20099a549f4d3782f22e6f0cfab3a7c2
biggy905 <alexander-a-bolgov@yandex.ru>
2023-02-22 11:01:47 +0300

    fix: add new fields for groups auction

--- src/common/groups/AuctionGroup.php
+++ src/common/groups/AuctionGroup.php
@@ -12,6 +12,10 @@ final class AuctionGroup
             'name' => $auction->name,
             'product' => $auction->product->name,
             'category' => $auction->product->category->name,
+            'start_time' => $auction->start_time,
+            'end_time' => $auction->end_time,
+            'start_date' => $auction->start_date,
+            'end_date' => $auction->end_date,
             'full_name' => $auction->user->getFullName() ?? 'Not installed',
             'created_at' => $auction->created_at,
         ];
